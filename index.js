e6ade4e892dbecdf86f6176e30007d8eae1d403c
Khaled <khalednaes3@gmail.com>
2023-03-16 13:53:46 +0300

    done!

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+//1
+const random = Math.floor(Math.random() * 10) + 1
+//2
+const num3 = 17
+const num4 =13
+const mod = num3 % num4
+//3
+const max = Math.max(1,20)
